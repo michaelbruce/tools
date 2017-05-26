4135e7ec8d63f85b8ad3dc62ec857512920952b4
Michael Bruce <mike@bearmetalcoding.com>
2017-05-26 17:27:30 +0100

    Emacs update

--- emacs-lib/find.el
+++ emacs-lib/find.el
@@ -1,14 +1,23 @@
 (defun find-something ()
-  (minibuffer-with-setup-hook
-      #'find--minibuffer-setup
-    (read-from-minibuffer
-     ">>> "
-     nil ;; initial input
-     nil ;; keymap
-     nil ;; ???
-     nil ;; history
-     )
+  (interactive)
+  (prog1
+      (minibuffer-with-setup-hook
+          #'find--minibuffer-setup
+        (read-from-minibuffer
+         ">>> "
+         nil ;; initial input
+         nil ;; keymap
+         nil ;; read
+         nil ;; history
+         ))
+    (search-with-current-input)
     ))
+
+(defun search-with-current-input ()
+  (interactive)
+  (shell-command-to-string
+   (concat "gfind ~/code/* -maxdepth 0 -printf \"%f\n\" | grep \"" current-value "\""))
+  )
 
 (defun find--minibuffer-setup ()
   (setq-local max-mini-window-height 10)
--- init.el
+++ init.el
@@ -1,17 +1,26 @@
 ;; init file
 
 (menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
 (setq inhibit-startup-message t)
-(blink-cursor-mode 0)
 
 (setq-default indent-tabs-mode nil) ;; always use spaces  
 (show-paren-mode 1)
 
+(if (display-graphic-p)
+    (progn
+      (add-to-list 'default-frame-alist '(alpha . (90 . 50)))
+      (scroll-bar-mode -1)
+      (tool-bar-mode -1)
+      (blink-cursor-mode -1)
+      (tooltip-mode nil)
+      (if (eq system-type 'darwin)
+          (set-frame-font "Inconsolata Bold 14")
+        (set-frame-font "Inconsolata Bold 15"))))
+
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "C-w") 'backward-kill-word)
 (global-set-key (kbd "M-[") 'hippie-expand)
+(global-set-key (kbd "C-q") 'find-something)
 
 (setq backup-directory-alist `(("." . "~/.saves")))
 
@@ -21,6 +30,7 @@
 
 (load-theme 'night t)
 (require 'utility)
+(require 'find)
 
 (autoload 'r-mode "r-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.\\(r\\|R\\)\\'" . r-mode))
