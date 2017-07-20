92bf60aec66b157185071a87d4429999801635ad
Michael Bruce <mike@bearmetalcoding.com>
2017-07-20 12:59:30 +0100

    Parengage open and delete [({" pairs

--- emacs-lib/parengage.el
+++ emacs-lib/parengage.el
@@ -3,22 +3,51 @@
   :lighter " parengage"
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "(") 'parengage-open-round)
-            (define-key map (kbd "<delete>") 'parengage-backward-char)
+            (define-key map (kbd "[") 'parengage-open-square)
+            (define-key map (kbd "{") 'parengage-open-curly)
+            (define-key map (kbd "\"") 'parengage-open-speech)
+            (define-key map (kbd "DEL") 'parengage-backward-char)
             map))
-
+ 
 (defun parengage-open-round ()
   (interactive)
   (progn
     (insert "()")
     (backward-char)))
 
+(defun parengage-open-square ()
+  (interactive)
+  (progn
+    (insert "[]")
+    (backward-char)))
+
+
+(defun parengage-open-curly ()
+  (interactive)
+  (progn
+    (insert "{}")
+    (backward-char)))
+
+(defun parengage-open-speech ()
+  (interactive)
+  (progn
+    (insert "\"\"")
+    (backward-char)))
+
 (defun parengage-backward-char ()
   (interactive)
-  (if (and (string= (string (char-after)) ")"))
-           (string= (string (char-before)) "("))
+  (if (or (and (string= (string (char-after)) ")")
+               (string= (string (char-before)) "("))
+          (and (string= (string (char-after)) "]")
+               (string= (string (char-before)) "["))
+          (and (string= (string (char-after)) "}")
+               (string= (string (char-before)) "{"))
+          (and (string= (string (char-after)) "\"")
+               (string= (string (char-before)) "\""))
+          )
       (progn
         (backward-delete-char 1)
         (delete-char 1))
-    (backward-char))
+    (backward-char 1)))
 
 (provide 'parengage)
